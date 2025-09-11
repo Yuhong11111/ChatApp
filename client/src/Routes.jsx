ad602b3e74212f011904be02f9543957b6683d8d
Yuhong11111 <nieyuhong2000@gmail.com>
2025-09-11 12:42:12 -0400

    add chat page

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,13 +1,14 @@
 import RegisterAndLoginForm from "./RegisterAndLoginForm";
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
+import Chat from "./Chat";
 
 export default function Routes() {
     const { username, id } = useContext(UserContext);
 
     if (username) {
         return (
-            <div>Logged in as {username} (ID: {id})</div>
+            <Chat />
         )
     }
     return (
